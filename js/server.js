1a07e6cd35fed415a8e403b320ec0bb889a80b6e
Boris Antunez <antunez.boris@gmail.com>
2024-06-24 00:31:33 -0400

    version 3.0

---
+++ js/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware para servir archivos estáticos desde el directorio 'public'
+app.use(express.static('public'));
+
+// Ruta básica
+app.get('/', (req, res) => {
+    res.send('Hola, mundo!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
